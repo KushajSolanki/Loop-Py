61693419c8d38e09bfa27ce3316ace6bf1d817e7
Kushaj Pavankumar Solanki <115155314+KushajSolanki@users.noreply.github.com>
2022-11-06 21:19:48 +0530

    Delete sum repeat - Copy.py

--- sum repeat - Copy.py
+++
@@ -1,8 +0,0 @@
-i = 1
-j = int(input("Enter number of times to repeat"))
-while(i<=j):
-    a = int(input("Enter a: "))
-    b = int(input("Enter b: "))
-    c = a + b
-    print("SUM :",c)
-    i = i+1
